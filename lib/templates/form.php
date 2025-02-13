a56160f8c90f655b42a91de1b1ab4232f00af1f8
Joshua Ellis <joshua@wideeyecreative.com>
2025-02-13 13:11:52 -0500

    add select input

--- lib/templates/form.php
+++ lib/templates/form.php
@@ -8,6 +8,13 @@
 ?>
 <form id="palette-form">
 	<h2>Create A New Palette</h2>
+	<select name="model">
+		<option value="default">Default</option>
+		<option value="ui">UI</option>
+		<option value="amelie_film">Graphic</option>
+		<option value="nature_photography">Interior</option>
+		<option value="kaguya_film">Fashion</option>
+	</select>
 	<button type="submit">Submit</button>
 </form>
 <div id="palette-results"></div>
